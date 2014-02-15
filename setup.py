007e28583b200c919cd6778e2cfbd465f9df2e25
Benjamin Staffin <ben@cloudscaling.com>
2014-02-14 23:33:56 -0800

    Reformat setup.py for readability.
    Change-Id: Id62d54730c06b71ee3bbf7c3704789b3ff7e75e6

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python
+
 from distutils.core import setup, Extension
 
 setup(
     ext_modules=[
-                Extension("kadmin",
-                    libraries       = ["krb5", "kadm5clnt"],
-                    library_dirs    = ["/usr/lib64/"], 
-                    include_dirs    = ["/usr/include/et/"],
-                    sources         = ["./kadmin.c", "./PyKAdminErrors.c", "./PyKAdminObject.c", "./PyKAdminPrincipalObject.c", "getdate.c"],
-                    
-                )
+        Extension(
+            "kadmin",
+            libraries=["krb5", "kadm5clnt"],
+            library_dirs=["/usr/lib64/"],
+            include_dirs=["/usr/include/et/"],
+            sources=[
+                "./kadmin.c",
+                "./PyKAdminErrors.c",
+                "./PyKAdminObject.c",
+                "./PyKAdminPrincipalObject.c",
+                "getdate.c"
+                ]
+            )
         ]
     )
     #/usr/lib64/libkadm5clnt_mit.so
